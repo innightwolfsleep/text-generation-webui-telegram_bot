d7cb9c9e1c1501e5a694bee877369684304611c9
innightwolfsleep <innightwolfsleep@yandex.ru>
2023-04-29 22:43:06 +0600

    fix config path

--- script.py
+++ script.py
@@ -6,7 +6,9 @@ import extensions.telegram_bot.generator_wrapper as gw
 def run_server():
     # create TelegramBotWrapper instance
     # by default, read parameters in telegram_config.cfg
-    tg_server = TelegramBotWrapper(generator_wrapper=gw)
+    tg_server = TelegramBotWrapper(
+        config_file_path="extensions/telegram_bot/telegram_config.cfg",
+        generator_wrapper=gw)
     # by default - read token from extensions/telegram_bot/telegram_token.txt
     tg_server.run_telegram_bot()
 
