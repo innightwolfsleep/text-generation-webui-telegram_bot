861c744a9a94c1f4cc88bd2f03a84b3fa32b22d7
innightwolfsleep <innightwolfsleep@yandex.ru>
2023-09-24 23:30:22 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import urllib3
 from deep_translator import GoogleTranslator as Translator
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, InputMediaAudio
 from telegram.constants import CHATACTION_TYPING
-from telegram.error import BadRequest
+from telegram.error import BadRequest, NetworkError
 from telegram.ext import (
     CallbackContext,
     Filters,
@@ -627,7 +627,7 @@ class TelegramBotWrapper:
 
     @backoff.on_exception(
         backoff.expo,
-        (urllib3.exceptions.HTTPError, urllib3.exceptions.ConnectTimeoutError),
+        NetworkError,
         max_time=60,
     )
     def send_sd_image(self, upd: Update, context: CallbackContext, answer, user_text):
@@ -645,7 +645,7 @@ class TelegramBotWrapper:
 
     @backoff.on_exception(
         backoff.expo,
-        (urllib3.exceptions.HTTPError, urllib3.exceptions.ConnectTimeoutError),
+        NetworkError,
         max_time=60,
     )
     def clean_last_message_markup(self, context: CallbackContext, chat_id: int):
@@ -658,7 +658,7 @@ class TelegramBotWrapper:
 
     @backoff.on_exception(
         backoff.expo,
-        (urllib3.exceptions.HTTPError, urllib3.exceptions.ConnectTimeoutError),
+        NetworkError,
         max_time=60,
     )
     def send_message(self, context: CallbackContext, chat_id: int, text: str):
@@ -700,7 +700,7 @@ class TelegramBotWrapper:
 
     @backoff.on_exception(
         backoff.expo,
-        (urllib3.exceptions.HTTPError, urllib3.exceptions.ConnectTimeoutError),
+        NetworkError,
         max_time=60,
     )
     def edit_message(
@@ -958,7 +958,8 @@ Language: {user.language}"""
         user = self.users[chat_id]
         # add pretty "retyping" to message text
         # remove last bot answer, read and remove last user reply
-        user_in = user.truncate_only_history()
+        user_in, _ = user.truncate()
+        user.msg_id.append(msg.message_id)
         # get answer and replace message text!
         answer, _ = self.generate_answer(user_in=user_in, chat_id=chat_id)
         self.edit_message(
